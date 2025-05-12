0860da2fb58bd00338367858578b2035e69c3289
Rashika2308 <rashikavidyarthi545@gmail.com>
2025-05-12 09:12:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function firstNonRepeatedChar(str) {
+	const charCount = {};
+	
+  for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
 
+  for (let char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+}
+
+const input = prompt("Enter a string");
+console.log(firstNonRepeatedChar(input));
